dac1b5b4022421dc597e3a3c3b5cb8125db3ab78
TheRealPaco98 <pasfid@live.it>
2022-12-05 14:35:30 +0100

    commit 1

---
+++ client/src/App.js
@@ -0,0 +1,64 @@
+import {useState, useEffect} from 'react';
+import { Web3Storage } from 'web3.storage/dist/bundle.esm.min.js';
+import './App.css';
+import {init, setHash} from './Web3Client';
+
+
+
+function App() {
+  
+  const client = new Web3Storage({ token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDU1NjlBRjI5MjI0ZDFDNjI1QUUwZDQzMjE0ZTE5Q0I4NzFhQUQ4RGEiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2Njk5MjEwOTUxNzUsIm5hbWUiOiJQcm9nZXR0byJ9.p9dqW2LWdCcYAwOoHgQKOMZvl8pRzYMzv7YX6L4_MhU' });
+
+  useEffect(() => {
+    init();
+  }, []); 
+
+  function setCid(cid){
+    setHash(cid).then(tx =>{
+      console.log(tx);
+    }).catch(err => {
+      console.log(err);
+      });
+  }
+  
+  const [file, setFile] = useState('')
+  
+  function storeFiles (file) {
+    const cid = client.put(file)
+    cid.then(value => {
+
+      console.log('stored files with cid:', value)
+      setCid(value);
+    });
+  }  
+
+  function handleCaptureFile(e) {
+    e.preventDefault();
+    console.log('capturing file...')
+    const fileInput = document.getElementById('csvFile');
+    const selectedFile = fileInput.files[0];
+    console.log(selectedFile);
+    setFile(fileInput.files)
+  }
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('You clicked submit.');
+    storeFiles(file)
+  }
+
+  return (
+    <div id="App" >
+        <div className="container">
+          <h1>Ciao</h1>
+          <p>Inserisci qui il tuo file .csv</p>
+          <form onSubmit={handleSubmit}>
+            <input type='file' id='csvFile' accept=".csv" onChange={handleCaptureFile}></input>
+            <input type='submit'></input>
+          </form>
+        </div>
+      </div>
+  );
+}
+
+export default App;
---
+++ client/src/Web3Client.js
@@ -0,0 +1,39 @@
+import StoreBuild from './truffle/build/contracts/Store.json';
+
+var Web3 = require('web3');
+let selectedAccount;
+let storeContract;
+let isInitialized = false;
+
+
+export const init = async () => {
+    let provider = window.ethereum;
+
+    if (typeof provider !== 'undefined') {
+    
+    provider
+    .request({method: 'eth_requestAccounts' })
+    .then((accounts) => {
+      selectedAccount = accounts[0];
+      console.log(`Selected account is ${selectedAccount}`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+    window.ethereum.on('accountsChanged', function (accounts){
+      selectedAccount = accounts[0];
+      console.log(`Selected account changed to ${selectedAccount}`);
+    });
+  }
+  const web3 = new Web3(provider);
+  const networkId = await web3.eth.net.getId();
+  storeContract = new web3.eth.Contract(StoreBuild.abi,StoreBuild.networks[networkId].address);
+  isInitialized = true;
+};
+
+export async function setHash(value) {
+  if (!isInitialized) {
+    await init();
+  }
+  return storeContract.methods.set(value).send({from: selectedAccount});
+}
